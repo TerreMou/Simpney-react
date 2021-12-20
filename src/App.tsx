83b5ff66e8d58f3358676378fbbbbe2ff23db8ed
terremou <terrence_mou@foxmail.com>
2021-12-21 00:23:41 +0800

    创建 Layout 组件和 Views 视图

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,29 @@
 import React from 'react';
 import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
-
-  );
-}
-
-function Tags() {
-  return (
-    <>
-      <div>Tags</div>
-    </>
-  );
-}
-
-function Money() {
-  return (
-    <>
-      <div>Money</div>
-    </>
-
-  );
-}
-
-function Statistics() {
-  return (
-    <>
-      <div>Statistics</div>
-    </>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>找不到页面</div>
   );
 }
 
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NoMatch() {
+  return (
+    <h2>找不到页面</h2>
+  );
+}
+
+export default NoMatch
