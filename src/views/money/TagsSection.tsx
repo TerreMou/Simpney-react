123a294f4668c06bd0edb851a3fbe40b0b027fa5
terremou <terrence_mou@foxmail.com>
2021-12-22 23:09:22 +0800

    完成TagsSection 的功能

--- src/views/money/TagsSection.tsx
+++ src/views/money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   flex-grow: 1;
   background: #ffffff;
   padding: 12px 16px;
@@ -19,6 +20,15 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      
+      &:hover{
+        cursor: pointer;
+      }
+
+      &.selected {
+        background: #6c71b1;
+        color: white;
+      }
     }
   }
 
@@ -31,5 +41,46 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(['购物', '吃饭', '交通', '其他']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('请输入标签名');
+    if (tagName !== null) {
+      if (tags.indexOf(tagName) >= 0) {
+        window.alert('标签名重复了');
+      } else {
+        tagName !== '' ?
+          setTags([...tags, tagName]) :
+          window.alert('标签名不能为空');
+      }
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag)
+    if (index >= 0) {
+      selectedTags.splice(index,1)
+      setSelectedTags([...selectedTags])
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const getClass = (tag:string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag => <li
+          onClick={() => onToggleTag(tag)}
+          className={getClass(tag)}
+          key={tag}>
+          {tag}
+        </li>)}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export {TagsSection};
