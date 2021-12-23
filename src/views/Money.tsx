924a10d21e97573fe505d26c412bdcfb98319251
terremou <terrence_mou@foxmail.com>
2021-12-23 16:44:39 +0800

    重构 NumberPad

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Layout from 'components/Layout';
 import styled from 'styled-components';
-import {CategorySection} from './money/CategorySection';
-import {NoteSection} from './money/NoteSection';
-import {NumberPadSection} from './money/NumberPadSection';
-import {TagsSection} from './money/TagsSection';
+import {CategorySection} from './Money/CategorySection';
+import {NoteSection} from './Money/NoteSection';
+import {NumberPadSection} from './Money/NumberPadSection';
+import {TagsSection} from './Money/TagsSection';
 
 const MyLayout = styled(Layout)`
   display: flex;
---
+++ src/views/Money/NumberPadSection.tsx
@@ -0,0 +1,53 @@
+import Icon from '../../components/Icon';
+import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import {generateOutput} from './NumberPadSection/generateOutput';
+
+const NumberPadSection = () => {
+  const [output, _setOutput] = useState<string>('0');
+  const setOutput = (output: string) => {
+    if (output.length>16) {
+      output = output.slice(0, 16)
+    } else if (output.length === 0) {
+      output = '0'
+    }
+    _setOutput(output);
+  };
+  const onClickPad = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) {return;}
+    if (text === 'OK') {
+      // TODO
+      console.log('确认')
+    }
+    if ('0123456789.'.split('').concat(['','清空']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output))
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div className="output">{output}</div>
+      <div className="pad" onClick={onClickPad}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="delete">
+          <Icon name="delete"/>
+        </button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button className="clear">清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export {NumberPadSection};
