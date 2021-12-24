69abbe2ed9dbc19feda96f4e5a110fa09604ef81
terremou <terrence_mou@foxmail.com>
2021-12-24 11:12:24 +0800

    添加 defaultTags

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,12 +1,15 @@
 import {useState} from 'react';
 import {createId} from 'lib/createId';
 
+const defaultTags = [
+  {id: createId(), name: '购物'},
+  {id: createId(), name: '吃饭'},
+  {id: createId(), name: '交通'},
+  {id: createId(), name: '其他'}
+];
+
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>([
-    {id: createId(), name: '购物'},
-    {id: createId(), name: '吃饭'},
-    {id: createId(), name: '交通'},
-    {id: createId(), name: '其他'}]);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   return {tags, setTags};
 };
 export {useTags};
