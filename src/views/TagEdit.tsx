6a84202bbb50e07f15d34f8384747a245590a25f
terremou <terrence_mou@foxmail.com>
2021-12-24 12:30:41 +0800

    封装 findTag 到 useTags 里，使用 useParams 拿到 url 里的 id

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
 
+type Params = {
+  id: string
+}
 const TagEdit: React.FC = () => {
+  const {findTag} = useTags();
+  let {id} = useParams<Params>(); // 拿到当前 url 的 id
+  const tag = findTag(parseInt(id));
+
   return (
-    <div>hi</div>
+    <div>{tag.name}</div>
   );
 };
 
