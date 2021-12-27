314c8a59c8bbeb8935bd144aab5299914efe5a16
terremou <terrence_mou@foxmail.com>
2021-12-27 15:47:00 +0800

    Tags 数据持久化：使用 localStorage

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,27 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {createId} from 'lib/createId';
-
-const defaultTags = [
-  {id: createId(), name: '购物'},
-  {id: createId(), name: '吃饭'},
-  {id: createId(), name: '交通'},
-  {id: createId(), name: '其他'}
-];
+import {useUpdate} from './hooks/useUpdate';
 
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>([]);
+
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: '购物'},
+        {id: createId(), name: '吃饭'},
+        {id: createId(), name: '交通'},
+        {id: createId(), name: '其他'}
+      ];
+    }
+    setTags(localTags)
+  }, []);
+
+  useUpdate(() => {
+    window.localStorage.setItem('tags', JSON.stringify(tags));
+  }, [tags]);
+
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
   const findTagIndex = (id: number) => {
     let result = -1;
